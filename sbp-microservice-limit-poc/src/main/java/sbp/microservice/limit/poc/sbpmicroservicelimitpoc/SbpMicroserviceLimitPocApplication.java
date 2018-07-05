c926efc69b96a709d461cb00d1e608c11c45ad77
Aarush <sabita.dhwaj@smartbitpixel.co.in>
2018-07-05 14:36:57 +0530

    POC for limit service

---
+++ sbp-microservice-limit-poc/src/main/java/sbp/microservice/limit/poc/sbpmicroservicelimitpoc/SbpMicroserviceLimitPocApplication.java
@@ -0,0 +1,12 @@
+package sbp.microservice.limit.poc.sbpmicroservicelimitpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbpMicroserviceLimitPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbpMicroserviceLimitPocApplication.class, args);
+	}
+}
---
+++ sbp-microservice-limit-poc/src/test/java/sbp/microservice/limit/poc/sbpmicroservicelimitpoc/SbpMicroserviceLimitPocApplicationTests.java
@@ -0,0 +1,16 @@
+package sbp.microservice.limit.poc.sbpmicroservicelimitpoc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SbpMicroserviceLimitPocApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
