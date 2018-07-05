b33a3cb01c9dc46cce579e9ae8736683835a6f9b
Aarush <sabita.dhwaj@smartbitpixel.co.in>
2018-07-05 13:57:45 +0530

    Limit service for poc1

---
+++ limit-poc1/src/main/java/sbp/f2f/limit/poc1/limitpoc1/LimitPoc1Application.java
@@ -0,0 +1,12 @@
+package sbp.f2f.limit.poc1.limitpoc1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitPoc1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimitPoc1Application.class, args);
+	}
+}
---
+++ limit-poc1/src/test/java/sbp/f2f/limit/poc1/limitpoc1/LimitPoc1ApplicationTests.java
@@ -0,0 +1,16 @@
+package sbp.f2f.limit.poc1.limitpoc1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LimitPoc1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
